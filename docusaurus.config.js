5ee94476b7f3b6c48df17dcc89d992048638863d
Ameng404 <ameng404@qq.com>
2021-08-14 22:58:48 +0800

    feat: add search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,6 +107,21 @@ module.exports = {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
+    algolia: {
+      apiKey: 'YOUR_API_KEY',
+      indexName: 'YOUR_INDEX_NAME',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: see doc section below
+      appId: 'YOUR_APP_ID',
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      //... other Algolia params
+    },
   },
   presets: [
     [
@@ -130,4 +145,8 @@ module.exports = {
       },
     ],
   ],
+  i18n: {
+    defaultLocale: 'zh-cn',
+    locales: ['en', 'zh-cn'],
+  },
 };
