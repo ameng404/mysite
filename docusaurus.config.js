5d800195d1a6361c4ecd5b5310279e287d2ea40b
yinmeng <yinmeng.1024@bytedance.com>
2022-06-10 17:51:39 +0800

    feat: 修改站点内容

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,34 +1,37 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const footer  = require('./footer');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: '阿猛的博客',
   tagline: '科技改变世界，技术成就未来！',
-  url: 'https://ameng.site',
+  url: 'https://iameng.cn',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
-  organizationName: 'ameng404', // Usually your GitHub org/user name.
-  projectName: 'ameng404.github.io', // Usually your repo name.
+  organizationName: 'iameng-cn', // Usually your GitHub org/user name.
+  projectName: 'iameng-cn.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: '阿猛的博客',
+      title: 'iameng.cn',
       logo: {
         alt: '阿猛的博客',
-        src: 'img/logo.png',
+        src: 'img/logo.jpeg',
       },
       items: [
         {
           type: 'doc',
           docId: 'intro',
           position: 'left',
-          label: '前端',
+          label: '文档',
         },
         { to: '/blog', label: '博客', position: 'left' },
+        { to: '/works', label: '作品', position: 'left' },
+        { to: '/about', label: '关于', position: 'left' },
         {
-          href: 'https://github.com/ameng404',
+          href: 'https://github.com/iameng-cn',
           label: 'GitHub',
           position: 'right',
         },
@@ -101,7 +104,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `<p>Copyright ©2018 - ${new Date().getFullYear()} 阿猛的文档, Built with Docusaurus.</p><p><a href="http://beian.miit.gov.cn/" >冀ICP备19004388号-1</a></p><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="/img/creative-commons-license-icon.png" /></a><br />本站所有内容遵循 <a rel="license" href="https://creativecommons.org/licenses/by-nc/4.0/deed.zh-Hans" >CC BY-NC 4.0 协议</a>，转载须注明署名和出处，且不可用于商业用途。若与其他同步平台协议冲突，以本网站为准。`,
+      copyright: footer,
     },
     prism: {
       theme: lightCodeTheme,
@@ -147,6 +150,6 @@ module.exports = {
   ],
   i18n: {
     defaultLocale: 'zh-cn',
-    locales: ['en', 'zh-cn'],
+    locales: ['zh-cn'],
   },
 };
