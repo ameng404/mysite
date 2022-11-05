1cc30cacaaf1b90a4bd127ae873ee9fde1c959c5
ameng404 <ameng404@qq.com>
2022-11-05 20:20:08 +0800

    feat: 重置

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/bytedance/Desktop/mysite/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/bytedance/Desktop/mysite/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/bytedance/Desktop/mysite/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/bytedance/Desktop/mysite/src/css/custom.css"),
-];
