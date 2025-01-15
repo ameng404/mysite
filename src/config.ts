4218925f2c6ad1c9c76b7c0a0b04d823af434001
yinmeng.1029 <yinmeng.1029@bytedance.com>
2025-01-15 18:59:05 +0800

    feat: 新增内容

---
+++ src/config.ts
@@ -0,0 +1,77 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: '阿猛的日常',
+  subtitle: '科技改变世界，技术成就未来！',
+  lang: 'zh_CN',        // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  themeColor: {
+    hue: 250,           // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,       // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: false,
+    src: '/assets/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center',          // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    credit: {
+      enable: false,             // Display the credit text of the banner image
+      text: '',                  // Credit text to be displayed
+      url: ''                    // (Optional) URL link to the original artwork or artist's page
+    }
+  },
+  toc: {
+    enable: true,           // Display the table of contents on the right side of the post
+    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    {
+      name: 'GitHub',
+      url: 'https://github.com/ameng404',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: '/assets/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'Ameng',
+  bio: '科技改变世界，技术成就未来！',
+  links: [
+    {
+      name: 'Email',
+      icon: 'material-symbols:mail-sharp',      // Visit https://icones.js.org/ for icon codes
+                                                // You will need to install the corresponding icon set if it's not already included
+                                                // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'mailto:ameng404@qq.com',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/ameng404',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
